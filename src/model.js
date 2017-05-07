a04560f3ac7c376b087b2b38b89845af30face2f
Jordan Santell <jordan@jsantell.com>
2017-05-06 22:15:19 -0700

    Remove extraneous code in Model leftover from Node

--- src/model.js
+++ src/model.js
@@ -6,31 +6,4 @@ export default class Model extends Node {
     this.geometry = geometry;
     this.material = material;
   }
-
-  add(object) {
-    // Not yet implementing mapping local space
-    // to world space based on ancestry in scene graph
-    if (!object instanceof Scene) {
-      throw new Error('not yet implemented');
-    }
-    this[CHILDREN].add(object);
-  }
-
-  remove(object) {
-    // Not yet implementing mapping local space
-    // to world space based on ancestry in scene graph
-    if (!object instanceof Scene) {
-      throw new Error('not yet implemented');
-    }
-    this[CHILDREN].delete(object);
-  }
-
-  getChildren() {
-    // Not yet implementing mapping local space
-    // to world space based on ancestry in scene graph
-    if (!object instanceof Scene) {
-      throw new Error('not yet implemented');
-    }
-    return this[CHILDREN].values();
-  }
 }
